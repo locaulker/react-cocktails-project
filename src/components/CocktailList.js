e0c71428ff7593c5613d35506c8b89d3e6710a9e
Lawrence <locaulker@gmail.com>
2021-01-29 07:00:11 -0500

    Retrieved fetched data, renamed and iterated list

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -4,6 +4,20 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext()
+  console.log(cocktails)
+
+  if (loading) {
+    return <Loading />
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h6 className='section-title'>
+        No Cocktails matched your searched criteria
+      </h6>
+    )
+  }
+
   return (
     <div>
       <h2>cocktail list component</h2>
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, saepe!
+        Vero iusto ut deleniti fugit tempora doloribus sequi nostrum, et facere
+        aliquid asperiores autem quia ducimus dolores sit fugiat mollitia?
+        Temporibus eaque quo, esse assumenda amet quis quidem nulla numquam
+        consequuntur beatae a enim voluptatibus, ipsam quaerat facilis officiis
+        magni!
+      </p>
+    </section>
   )
 }
 
